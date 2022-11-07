d3a2f47b8dd9ce9d80f88197873f4c5bb35c4d4b
rganis <rganis@wgu.edu>
2022-11-06 21:14:10 -0500

    got eurusd data from yfinance

--- FXStrat/FXStrat.py
+++ FXStrat/FXStrat.py
@@ -1,34 +1,12 @@
-import requests as req
-
-print("requests")
-
-
-from tda.auth import easy_client
-from tda.client import Client
-from tda.streaming import StreamClient
-
-import asyncio
-import json
-
-client = easy_client(
-        api_key='APIKEY',
-        redirect_uri='https://localhost/',
-        token_path='/tmp/token.json')
-stream_client = StreamClient(client, account_id=1234567890)
-
-async def read_stream():
-    await stream_client.login()
-    await stream_client.quality_of_service(StreamClient.QOSLevel.EXPRESS)
-
-    def print_message(message):
-      print(json.dumps(message, indent=4))
-
-    # Always add handlers before subscribing because many streams start sending
-    # data immediately after success, and messages with no handlers are dropped.
-    stream_client.add_nasdaq_book_handler(print_message)
-    await stream_client.nasdaq_book_subs(['GOOG'])
-
-    while True:
-        await stream_client.handle_message()
-
-asyncio.run(read_stream())
+# Description: This script will download the EURUSD data from Yahoo Finance
+#
+# do the imports
+import pandas as pd;
+import numpy as np;
+import yfinance as yf;
+import datetime as dt;
+# get the data
+aapl = yf.Ticker("EURUSD=X")
+# get the historical prices for this ticker
+hist = aapl.history(period="1mo")
+print(hist)
