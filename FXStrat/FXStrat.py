1787b3778e0a83c6c8fd4c8d8e7c8a23e93ea23a
rganis <rganis@wgu.edu>
2022-11-06 21:36:28 -0500

    renamed variable and deleted unused import

--- FXStrat/FXStrat.py
+++ FXStrat/FXStrat.py
@@ -2,13 +2,12 @@
 #
 # do the imports
 import pandas as pd;
-from pandas import read_csv;
 import numpy as np;
 import yfinance as yf;
 import datetime as dt;
 # get the data
-aapl = yf.Ticker("EURUSD=X")
+eu = yf.Ticker("EURUSD=X")
 # get the historical prices for this ticker
 # 1mo is 1 calendar month, approx 20 trading days
-hist = aapl.history(period="1mo")
+hist = eu.history(period="1mo")
 print(hist)
