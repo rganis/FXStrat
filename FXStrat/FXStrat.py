8b15d524f253c46e7834e2a7d4c3fbaccc6cc30d
rganis <rganis@wgu.edu>
2022-11-07 20:43:41 -0500

    try to calculate on loop

--- FXStrat/FXStrat.py
+++ FXStrat/FXStrat.py
@@ -6,8 +6,18 @@ import numpy as np;
 import yfinance as yf;
 import datetime as dt;
 # get the data
-eu = yf.Ticker("EURUSD=X")
+tickers = ["EURUSD=X", "GBPUSD=X"]
+
+for y in tickers:
+    tix = yf.Ticker(tickers[y])
+    hist = tix.history(period="1mo")
+    df = pd.DataFrame(hist)
+    c = np.average(df['Close'])
+    print(c)
+#tix = yf.Ticker("EURUSD=X")
 # get the historical prices for this ticker
 # 1mo is 1 calendar month, approx 20 trading days
-hist = eu.history(period="1mo")
-print(hist)
+#hist = eu.history(period="1mo")
+#df = pd.DataFrame(hist)
+#c = np.average(df['Close'])
+#print(c)
